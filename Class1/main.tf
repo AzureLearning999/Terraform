c2e1f14b1be87972090394c9510c4a1d9e7b349b
AzureSikho <143492892+AzureSikho@users.noreply.github.com>
2023-08-29 10:05:59 +0000

    update

---
+++ Class1/main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "MonuRG" {
+  name     = "Start1"
+  location = "Southeast Asia"
+}
---
+++ Class1/variables.tf
@@ -0,0 +1,6 @@
+/*
+variable "location" {
+  type = []
+  description = region where resource will be deployed
+  }
+  */
